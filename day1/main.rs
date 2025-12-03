aadef4438d6064388949071614d87fe89ac7e6bb
ignite <juliette.or@icloud.com>
2025-12-03 09:54:49 +0100

    first push

---
+++ day1/main.rs
@@ -0,0 +1,71 @@
+
+
+
+
+// 
+
+open file 
+read file
+first letter in letter
+second part dans number 
+
+
+loop trew list 
+
+sum = 0
+
++ 5 
++ 49
+- 42 
+
+
+tableau d'int de 0 a 99
+[99, 98, 97, 96, 95, 94, ]
+
+
+
+94, 
+
+
+//exemple 
+/* 
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+Following these rotations would cause the dial to move as follows:
+
+The dial starts by pointing at 50.
+The dial is rotated L68 to point at 82.
+The dial is rotated L30 to point at 52.
+The dial is rotated R48 to point at 0.
+The dial is rotated L5 to point at 95.
+The dial is rotated R60 to point at 55.
+The dial is rotated L55 to point at 0.
+The dial is rotated L1 to point at 99.
+The dial is rotated L99 to point at 0.
+The dial is rotated R14 to point at 14.
+The dial is rotated L82 to point at 32.
+/*
+
+
+make an array from 99 to 0 
+if l go right 
+ig r go left
+
+
+exemple if the index if at 50
+
+go right so since the array is from 99 to 0 
+for  L68
+the pointeur would go to O it would make 50 right and then goes backto 0 at the begining
+it wil rest 18 moving riight
+so 99 98 97 etc to 82
+
+then
+
